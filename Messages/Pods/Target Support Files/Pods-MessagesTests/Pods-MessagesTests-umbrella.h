6ef0d5ccafffcb9d97d871b49c64b1a5268e98a2
Jaime Laino <jaimelaino@JLainoG.local>
2017-03-06 15:36:41 -0500

    - configure Firebase pod

---
+++ Messages/Pods/Target Support Files/Pods-MessagesTests/Pods-MessagesTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MessagesTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MessagesTestsVersionString[];
+
---
+++ Messages/Pods/Target Support Files/Pods-MessagesUITests/Pods-MessagesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessagesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MessagesUITests
+@end
