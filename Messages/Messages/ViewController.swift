62ac0f5d1dfd574195ddbbc6c802fae7c136c292
Jaime Laino <jaimelaino@JLainoG.local>
2017-03-06 15:20:32 -0500

    - Create XCode Proyect

---
+++ Messages/Messages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Jaime Laino on 3/6/17.
+//  Copyright © 2017 JLainoG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
