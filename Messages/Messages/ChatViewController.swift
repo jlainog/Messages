92e14437793030217ee4dc43003403ca9a62d4a2
Stephany Lara Jimenez <stlaraj@gmail.com>
2017-03-08 11:07:34 -0500

    Adicion POD JSQMessageViewController

---
+++ Messages/Messages/ChatViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatViewController.swift
+//  Messages
+//
+//  Created by Stephany Lara Jimenez on 3/8/17.
+//  Copyright © 2017 JLainoG. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        <#code#>
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didDeleteMessageAt indexPath: IndexPath!) {
+        <#code#>
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        <#code#>
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        <#code#>
+    }
+}
+
