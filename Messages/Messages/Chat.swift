f5ed8b13fbffaa3c0394fdcc0cbcfd7129b894f3
Luis Ramirez <lramirezsuarez@gmail.com>
2017-03-09 13:49:25 -0500

    #6 - Corrections to Chat model.

--- Messages/Messages/Chat.swift
+++ Messages/Messages/Chat.swift
@@ -13,27 +13,13 @@ enum MessageType {
 }
 
 protocol ChatInfo : Parseable {
-    var channelId : Int { get }
-    var userId : Int { get }
     var messages : [Message] { get }
-    var messageType : MessageType { get }
-    var time : NSNumber { get }
 }
 
 struct Chat : ChatInfo {
-    var channelId: Int
-    var userId: Int
     var messages: [Message]
-    var messageType: MessageType
-    var time: NSNumber
     
     init(json: [String : Any]) {
-        self.channelId = json["chatId"] as? Int ?? 0
-        self.userId = json["userId"] as? Int ?? 0
-        self.messages = json["message"] as? [Message] ?? [Message]()
-        self.messageType = json["messageType"] as? MessageType ?? .text
-        self.time = (json["time"] as? NSNumber)!
+        self.messages = json["messages"] as? [Message] ?? [Message]()
     }
 }
-
-
